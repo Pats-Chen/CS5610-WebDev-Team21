07860c15aad98b373fbd6ef52396735161a01155
mozhi21 <mozhi21@gmail.com>
2023-02-24 18:31:29 -0800

    tried google map API in GoogleMapTest.js

---
+++ src/GoogleMapTest.js
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+function Home() {
+    // console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    });
+
+    if (!isLoaded) return <div>Loading...</div>;
+    return <Map />;
+}
+
+function Map() {
+    const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+
+    return (
+        <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+            <Marker position={center} />
+        </GoogleMap>
+    );
+}
+
+export default Home
