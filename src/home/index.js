60f31b729dc3db56391b7dae95141ee5aa81964d
mozhi21 <mozhi21@gmail.com>
2023-04-05 17:04:00 -0700

    trip summary info in .json, draft of home page and trip summary view

--- src/home/index.js
+++ src/home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./homepage.css";
 import homeCarousel from "./home-carousel";
 import HomeCarousel from "./home-carousel";
+import TripSummaryList from "../trip-summary-view/trip-summary-list.js";
 function Home(){
     return(
         <main>
@@ -64,6 +65,7 @@ function Home(){
             </header>
 
             <HomeCarousel></HomeCarousel>
+
             <div className="row  featurette container-fluid bg-opacity-50" >
                 <div className="col-md-9" >
                     <h2 className="featurette-heading fw-normal lh-1">Plan a Trip
@@ -73,57 +75,12 @@ function Home(){
                 </div>
             </div>
 
-            <div className="container marketing">
-            <div className="row">
-                <div className="col-lg-4">
-                    <svg className="bd-placeholder-img rounded-circle" width="140" height="140"
-                         xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140"
-                         preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#777"/>
-                        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                    </svg>
-
-                    <h2 className="fw-normal">Heading</h2>
-                    <p>Some representative placeholder content for the three columns of text below the carousel. This is the
-                        first column.</p>
-                    <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
-                </div>
-
-                <div className="col-lg-4">
-                    <svg className="bd-placeholder-img rounded-circle" width="140" height="140"
-                         xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140"
-                         preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#777"/>
-                        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                    </svg>
-
-                    <h2 className="fw-normal">Heading</h2>
-                    <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second
-                        column.</p>
-                    <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
-                </div>
-
-                <div className="col-lg-4">
-                    <svg className="bd-placeholder-img rounded-circle" width="140" height="140"
-                         xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140"
-                         preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#777"/>
-                        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                    </svg>
-
-                    <h2 className="fw-normal">Heading</h2>
-                    <p>And lastly this, the third column of representative placeholder content.</p>
-                    <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
-                </div>
-            </div>
-        </div>
-
+            <TripSummaryList/>
 
             <footer className="container">
             <p className="float-end"><a href="#">Back to top</a></p>
             <p>&copy; Team21. CS5610. NEU. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-        </footer>
-
+            </footer>
 
         </main>
     )
