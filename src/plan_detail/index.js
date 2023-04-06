c9ed3bc5b654d30e8afb0da836aa238060b5fade
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2023-04-06 00:04:34 -0400

    map is done

--- src/plan_detail/index.js
+++ src/plan_detail/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import ItineraryList from "./itinerary-list";
+import ItineraryMap from "./itinerary-map";
+import {useLoadScript} from "@react-google-maps/api";
+import './index.css';
 
 const PlanDetailComponent = () => {
+    const { isLoaded } = useLoadScript({
+                                           googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY});
+    if (!isLoaded) return <div>Loading...</div>
     return (
         <>
-            <h1>test</h1>
+            <div className="row">
+                <div className="col-3">
+                    <h1>Itinerary</h1>
+                    <ItineraryList/>
+                </div>
+                <div className="col-9">
+                    <ItineraryMap/>
+                </div>
+            </div>
         </>
     )
 };
---
+++ src/plan_detail/itinerary-list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ItineraryList = () => {
+    return (
+        <>
+            <h1>ItineraryList</h1>
+        </>
+    )
+}
+
+export default ItineraryList;
