dfffc001ebad42ffeeb002adacc76cff150d9273
Mozhi Shen <mzshen21@gmail.com>
2023-04-16 17:56:14 -0700

    User in plan detail (#15)
    * add navigation to plan Owners profile, now Owers id is hard coded.
    
    ---------
    
    Co-authored-by: mozhi21 <mozhi21@gmail.com>

--- src/plan-detail/index.js
+++ src/plan-detail/index.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ItineraryList from "./itinerary-list";
 import ItineraryMap from "./itinerary-map";
 import {useLoadScript} from "@react-google-maps/api";
 import './index.css';
+import {getUserProfile} from "../services/user-service";
+import {Link} from "react-router-dom";
 
 const PlanDetailComponent = () => {
     const { isLoaded } = useLoadScript({
                                            googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY});
+    // hardcode demoID：
+    const planOwnerId ="643c5e65a928059058979dc4";
+    const [planOwner, setPlanOwner]= useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+                const userProfile = await getUserProfile(planOwnerId);
+                setPlanOwner(userProfile);
+        }
+        fetchData();
+    }, []);
     if (!isLoaded) return <div>Loading...</div>
+
     return (
         <>
             <div className="container row bg-light rounded-top-2 ps-5 m-0">
@@ -17,6 +31,21 @@ const PlanDetailComponent = () => {
                 </div>
                 <div className="col-2 bg-light"></div>
             </div>
+            {/*Placeholder for navigation to Owner's profile*/}
+            <div className="container row bg-light rounded-top-2 ps-5 m-0">
+                <div className="col-2 bg-light"></div>
+                <div className="col-8 bg-light">
+                    {/*must use planOwner &&*/}
+                    {planOwner && ( <>
+                        <label className="form-label" htmlFor=""><h4>Plan Owner: {planOwner.username}</h4></label>
+                        <Link to={`/travelAdvisor/profile/${planOwnerId}`}
+                              className="text-decoration-none">
+                            <h4>View Profile</h4>
+                        </Link></>)}
+                </div>
+                <div className="col-2 bg-light"></div>
+            </div>
+            {/*Placeholder ends here*/}
             <div className="container bg-light pb-2 rounded-bottom-2">
                 <ItineraryList/>
             </div>
