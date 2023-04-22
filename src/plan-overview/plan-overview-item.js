e7897329ee5e11bec86d88934c4c2d05e08a4528
mozhi21 <mozhi21@gmail.com>
2023-04-22 10:26:41 -0700

    now plan over view has pictures

--- src/plan-overview/plan-overview-item.js
+++ src/plan-overview/plan-overview-item.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Link} from "react-router-dom";
 import {getUserProfile} from "../services/user-service";
 
@@ -20,16 +20,44 @@ const PlanOverviewItem = (
     }
 ) => {
     const [planOwner,setPlanOwner] = useState(null);
+    const [placeId, setPlaceId] = useState(null);
+    const [placeDetails, setPlaceDetails] = useState(null);
+
     useEffect(()=>{
         getUserProfile(planOverview.planCreator).then((result)=>setPlanOwner(result))
+        setPlaceId(planOverview.locations[0].placeId);
+        // console.log(planOverview.locations[0].placeId)
     },[])
 
+    useEffect(() => {
+        const fetchPlaceURL = async () => {
+            const map = new window.google.maps.Map(document.createElement('div'));
+            const service = new window.google.maps.places.PlacesService(map);
+            const request = {placeId: placeId, fields: ['photos'],};
+
+            service.getDetails(request, (place, status) => {
+                if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                    setPlaceDetails(place);
+                }
+            });
+        };
+        fetchPlaceURL();
+    }, [placeId]);
+
+    if (!placeDetails) {
+        return <div>Loading...</div>;
+    }
+    const photoUrl =
+        placeDetails.photos && placeDetails.photos.length > 0
+            ? placeDetails.photos[0].getUrl()
+            : null;
     return (
         <>
             <div className="col-md-4">
                 <div className="card mb-4 box-shadow">
                     <img className="card-img-top"
-                         src={`${process.env.PUBLIC_URL}/img/${planOverview.planCover}`}
+                         // src={`${process.env.PUBLIC_URL}/img/${planOverview.planCover}`}
+                        src={photoUrl}
                          alt={`${planOverview.planCover}`}
                          style={{height: "225px", width: "100%", display: "block"}}/>
                         <div className="card-body">
